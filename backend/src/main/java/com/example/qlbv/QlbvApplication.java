48c4f677f8e2e00aef7b9dbde0e365767b381ec0
RETASTE Developer <developer@retaste.com>
2026-05-31 03:14:27 +0700

    Add PostgreSQLDialect + debug env var logging in QlbvApplication

--- backend/src/main/java/com/example/qlbv/QlbvApplication.java
+++ backend/src/main/java/com/example/qlbv/QlbvApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QlbvApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(QlbvApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("DEBUG SPRING_DATASOURCE_URL: " + System.getenv("SPRING_DATASOURCE_URL"));
+        System.out.println("DEBUG SPRING_DATASOURCE_USERNAME: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+        System.out.println("DEBUG SPRING_PROFILES_ACTIVE: " + System.getenv("SPRING_PROFILES_ACTIVE"));
+        SpringApplication.run(QlbvApplication.class, args);
+    }
+}
 
 }
